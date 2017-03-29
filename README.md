b17fa6aa4b2d25900158b80ae8f7e5f98a0c836c
Christopher Klamm <klamm@ukp.informatik.tu-darmstadt.de>
2017-03-29 13:06:41 +0200

    added some comments and a little description about the project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # SchreibMap
+
+Here you can find a simple map (+ CSV) of some writing centres in germany, austria and switzerland. Most of the data based on "Schreibwerkstatt Universität Stuttgart" [1]. Additionally I added geographic coordinates and updated all links.
+
+If you want to play around with the data feel free to use it.
+
+Have fun!
+
+
+[1] "http://www.schreibwerkstatt.uni-stuttgart.de/wir-ueber-uns/Vernetzung/Schreibzentren_im_deutschsprachigen_Raum.html"
--- get_schreibzentren.py
+++ get_schreibzentren.py
@@ -1,54 +1,43 @@
-# Webcrawler
-# Version 0.1
-# Author: Christopher Klamm
-# 11.3.2017
+# Webcrawler for writing centre in germany, austria and switzerland
+# Version 0.2
+# Author: Chkla
+# 29.3.2017
 
-#import urllib2
 import urllib.request
 from bs4 import BeautifulSoup
 import geocoder
 import csv
 
-
-grippe = "http://www.schreibwerkstatt.uni-stuttgart.de/wir-ueber-uns/Vernetzung/Schreibzentren_im_deutschsprachigen_Raum.html"
-#page = urllib2.urlopen(grippe)
+# open the url e.g. schreibwerkstatt.uni-stuttgart.de
 page = urllib.request.urlopen('http://www.schreibwerkstatt.uni-stuttgart.de/wir-ueber-uns/Vernetzung/Schreibzentren_im_deutschsprachigen_Raum.html')
 soup = BeautifulSoup(page, "lxml")
 
+# list to save schoolname, typ, link and the coordinates of each entitiy
 data = []
-all_news = soup.find("table", { "class" : "belted" })
-table = all_news
+
+# select just the table
+table = soup.find("table", { "class" : "belted" })
 table_body = table.find('tbody')
 rows = table_body.find_all('tr')
 
-i = 0
+# interate through all rows
 for row in rows:
+	# select one row
     cols = row.find_all('td')
+
+    # get the link
     articles = {i.a['href']: i.text.strip() for i in cols if i.a}
-    #print(articles.keys())
+
+    # get the text
     cols = [ele.text.strip() for ele in cols]
+
+    # add schoolname, typ, link and the coordinates of each entitiy to the list
     data.append([ele for ele in cols if ele] + list(articles.keys()) + list(geocoder.google(cols[1]).latlng))
 
+# open/ creat a new csv file
 writer = csv.writer(open("writing_center.csv", "w"))
+# specify the rows
 writer.writerow(["City", "SchoolName", "Typ", "Url", "GeoLat", "GeoLng"])
+
+# save all data
 writer.writerows(data)
-
-#gesamt = []
-
-#for d in data:
-#	gesamt.append([d[1], d[2], geocoder.google(d[0]).latlng])
-
-
-#for g in gesamt:
-#	print(g)
-
-#g = geocoder.google('Darmstadt')
-#print(g.latlng)
-
-#articles = {i.a['href']: i.text.strip() for i in all_news if i.a}
-#articles = all_news
-#for article in articles:
-	#s = '{title}: {url}: {date}'.format(title=articles[article],url='https://de.grippenet.ch/de/'+article, date=article[:10])
-	#title = articles[article]
-#	url = article
-#	print(url)
